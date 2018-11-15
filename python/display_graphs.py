43496d18ba49790423cc676a2d08d5f1c1464ed2
Pljosan <ana.bajic13@gmail.com>
2018-11-15 23:21:43 +0100

    fixed image scaling

--- python/display_graphs.py
+++ python/display_graphs.py
@@ -3,6 +3,28 @@ import pydot;
 from Tkinter import *;
 from PIL import Image, ImageTk;
 
+def createButton(root, canvas, text, drawer, location):
+	if text == "Previous":
+		button = Button(root, text = text, command = drawer.draw_previous, anchor = W)
+	elif text == "Next":
+		button = Button(root, text = text, command = drawer.draw_new, anchor = W)
+	elif text == "Loop":
+		button = Button(root, text = text, command = drawer.draw_new_timer, anchor = W)
+	else:
+		button = Button(root, text = text, command = root.quit, anchor = W)
+	button.configure(width = 10, activebackground = "#33B5E5", relief = RAISED)
+	button_window = canvas.create_window(location, root.winfo_height(), anchor=SW, window=button)
+
+def setImageSize(percent, windowWidth, windowHeight, imageWidth, imageHeight):
+	baseheight = (percent * windowHeight)/12;
+	hpercent = (baseheight/float(imageHeight));
+	wsize = int((float(imageWidth)*float(hpercent)));
+	if wsize > 3*windowWidth/5:
+		return setImageSize(percent - 1, windowWidth, windowHeight, imageWidth, imageHeight);
+	else:
+		return (baseheight, wsize);
+
+
 if len(sys.argv) != 2:
 	print "fali parametar!";
 	exit();
@@ -16,18 +38,18 @@ class DrawImage:
             self.images = images;
             self.canvas = canvas;
 	    self.counter = 0;
-            #self.image = self.images.pop(0);
 	    self.image = self.images[self.counter];
             self.canvas.create_image((self.x, self.y), tags = "deleteMe", image = self.image, anchor='center');
+	    print "drew image at i = " , self.counter
             
         def draw_new(self):
 	    if (self.counter + 1 <= len(self.images) - 1):
 	    	self.counter += 1
             if self.counter < len(self.images):
-                #self.image = self.images.pop(0);
 		self.image = self.images[self.counter];
                 self.canvas.delete("deleteMe");
                 self.canvas.create_image((self.x, self.y), tags = "deleteMe", image = self.image, anchor='center');
+                print "drew image at i = " , self.counter
 
         def draw_previous(self):
 	    if (self.counter - 1 >= 0):
@@ -36,7 +58,8 @@ class DrawImage:
 		self.image = self.images[self.counter];
                 self.canvas.delete("deleteMe");
                 self.canvas.create_image((self.x, self.y), tags = "deleteMe", image = self.image, anchor='center');
-                
+                print "drew image at i = " , self.counter
+
         def draw_new_timer(self):
 	    if (self.counter + 1 <= len(self.images) - 1):
 	    	self.counter += 1
@@ -44,7 +67,8 @@ class DrawImage:
                 self.image = self.images[self.counter];
 		self.canvas.delete("deleteMe");
                 self.canvas.create_image((self.x, self.y), tags = "deleteMe", image = self.image, anchor='center');
-                self.canvas.after(5000, self.draw_new);
+                print "drew image at i = " , self.counter
+                self.canvas.after(5000, self.draw_new_timer);
 
 root = Tk();
 root.title("Balls");
@@ -59,6 +83,7 @@ canvas.config(background = "white");
 images = [];
 firstLastImage = [];
 
+
 for i in range(1, int(numberOfTxtFiles)):
 	image = Image.open("../files/png_files/graph" + str(i) + ".png");
 	if i == 1 or i == 2:
@@ -69,9 +94,12 @@ for i in range(1, int(numberOfTxtFiles)):
 		img = ImageTk.PhotoImage(img);
 		firstLastImage.append(img);
 	
-	baseheight = 11*root.winfo_height()/12;
-	hpercent = (baseheight/float(image.size[1]));
-	wsize = int((float(image.size[0])*float(hpercent)));
+	initialPercent = 11
+	(baseheight, wsize) = setImageSize(initialPercent, root.winfo_width(), root.winfo_height(), image.size[0], image.size[1]);
+#	baseheight = 11*root.winfo_height()/12;
+#	hpercent = (baseheight/float(image.size[1]));
+#	wsize = int((float(image.size[0])*float(hpercent)));
+		
 	image = image.resize((wsize, baseheight), Image.ANTIALIAS);
 
 	image = ImageTk.PhotoImage(image);
@@ -84,20 +112,9 @@ canvas.create_image((root.winfo_width(), 0), image = firstLastImage[1], anchor='
 
 drawer = DrawImage(canvas, images, root.winfo_width()/2, root.winfo_height()/2);
 
-button0 = Button(root, text = "Previous", command = drawer.draw_previous, anchor = W)
-button0.configure(width = 10, activebackground = "#33B5E5", relief = RAISED)
-button0_window = canvas.create_window(0, root.winfo_height(), anchor=SW, window=button0)
-
-button1 = Button(root, text = "Next", command = drawer.draw_new, anchor = W)
-button1.configure(width = 10, activebackground = "#33B5E5", relief = RAISED)
-button1_window = canvas.create_window(120, root.winfo_height(), anchor=SW, window=button1)
-
-button2 = Button(root, text = "Loop", command = drawer.draw_new_timer, anchor = W)
-button2.configure(width = 10, activebackground = "#33B5E5", relief = RAISED)
-button2_window = canvas.create_window(240, root.winfo_height(), anchor=SW, window=button2)
-
-button3 = Button(root, text = "Exit", command = root.quit, anchor = W)
-button3.configure(width = 10, activebackground = "#33B5E5", relief = RAISED)
-button3_window = canvas.create_window(360, root.winfo_height(), anchor=SW, window=button3)
+createButton(root, canvas, "Previous", drawer, 0);
+createButton(root, canvas, "Next", drawer, 120);
+createButton(root, canvas, "Loop", drawer, 240);
+createButton(root, canvas, "Exit", drawer, 360);
 
 root.mainloop();
